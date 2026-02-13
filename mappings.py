cc8bee47b3f9f55744a88e6eedf7ec12c056408d
saarthaksharma2002 <saarthak.sharma@innovaccer.com>
2026-02-13 12:32:59 +0530

    Added power bi input table mapping

--- mappings.py
+++ mappings.py
@@ -2,8 +2,6 @@
 import streamlit as st
 import json
 
-# Streamlit Cloud automatically loads these from the dashboard secrets
-# No need for os.getenv or load_dotenv()
 
 try:
     # Access the secret as if it were an environment variable or dictionary
@@ -27,6 +25,24 @@ CATEGORY_CONFIG = {
     "None Available": "_patients_with_neither_contact_nor_email"
 }
 
+POWERBI_ORDER = [
+    "total_attributed_lives", "den_hpv_9_17", "num_hpv_9_17", "den_shingles_50_59", 
+    "num_shingles_50_59", "den_shingles_60_64", "num_shingles_60_64", 
+    "den_shingles_65_plus", "num_shingles_65_plus", "den_rsv_covid_60_64", 
+    "num_rsv_covid_60_64", "den_rsv_covid_65_74", "num_rsv_covid_65_74", 
+    "den_rsv_covid_75_plus", "num_rsv_covid_75_plus", "den_pneumococcal_50_plus", 
+    "num_pneumococcal_50_plus", "den_pneumococcal_50_64", "num_pneumococcal_50_64", 
+    "den_pneumococcal_65_plus", "num_pneumococcal_65_plus", "den_influenza_50_plus", 
+    "num_influenza_50_plus", "den_covid_65_plus", "num_covid_65_plus", 
+    "den_rsv_60_plus", "num_rsv_60_plus", "den_meningococcal_16_18", 
+    "num_men_b_acwy_abcwy_16_18", "den_meningococcal_19_23", "num_men_b_acwy_abcwy_19_23", 
+    "num_men_acwy_abcwy_16_18", "num_men_acwy_abcwy_19_23", "num_men_b_16_18", 
+    "num_men_b_19_23", "num_men_acwy_16_18", "num_men_acwy_19_23", 
+    "num_men_abcwy_16_18", "num_men_abcwy_19_23", "a_b_den_paxlovid", 
+    "b_num_paxlovid", "count_shingles_ls", "count_men_acwy_actual", 
+    "count_men_b_ls", "common_patients_eligible_for_both"
+]
+
 VACCINE_ORDER_PATTERNS = [
     "total_attributed_lives", "age_16", "age_50", "hpv_9_17", "shingles_50_59", 
     "shingles_60_64", "shingles_65_plus", "rsv_covid_60_64", "rsv_covid_65_74", 
@@ -37,5 +53,6 @@ VACCINE_ORDER_PATTERNS = [
     "men_acwy_actual", "men_b_actual", "men_b_ls"
 ]
 AGE_COLUMNS = [str(i) for i in range(101)]    
+
 
 
