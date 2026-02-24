a3300001c2d2d445e9e7250c1c068c7ea416ef95
saarthaksharma2002 <saarthak.sharma@innovaccer.com>
2026-02-24 16:25:39 +0530

    Update mappings.py for feb data refresh

--- mappings.py
+++ mappings.py
@@ -46,13 +46,15 @@ POWERBI_ORDER = [
 VACCINE_ORDER_PATTERNS = [
     "total_attributed_lives", "age_16", "age_50", "hpv_9_17", "shingles_50_59", 
     "shingles_60_64", "shingles_65_plus", "rsv_covid_60_64", "rsv_covid_65_74", 
-    "rsv_covid_75_plus", "pneumococcal_50_plus", "pneumococcal_50_64", 
-    "pneumococcal_65_plus", "influenza_50_plus", "covid_65_plus", "rsv_60_plus",
-    "men_b_acwy_abcwy_16_18", "men_acwy_abcwy_16_18", "men_b_16_18", "men_acwy_16_18",
-    "men_abcwy_16_18", "paxlovid", "shingles_actual", "shingles_ls", 
-    "men_acwy_actual", "men_b_actual", "men_b_ls"
+    "rsv_covid_75_plus", "pnuemococcal_50_plus","pneumococcal_50_plus", "pnuemococcal_50_64","pneumococcal_50_64", 
+    "pnuemococcal_65_plus","pneumococcal_65_plus", "influenza_50_plus","influenza_50", "covid_65_plus", "rsv_60_plus",
+    "men_b_acwy_abcwy_16_18","men_b_acwy_abcwy_19_23", "men_acwy_abcwy_16_18","men_acwy_abcwy_19_23", "men_b_16_18","men_b_19_23", "men_acwy_16_18",
+    ,"men_acwy_19_23",
+    "men_abcwy_16_18","men_abcwy_19_23", "paxlovid", "shingles_actual", "shingles_ls", 
+    ,"men_acwy_actual", "men_b_actual", "men_b_ls"
 ]
 AGE_COLUMNS = [str(i) for i in range(101)]    
+
 
 
 
